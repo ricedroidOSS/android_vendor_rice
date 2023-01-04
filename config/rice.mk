9340b9cf578c86daf595e69b05506a6064884017
Pranav Vashi <neobuddy89@gmail.com>
2023-01-04 12:09:58 +0800

    config: Include crdroid faceunlock
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/rice.mk
+++ config/rice.mk
@@ -76,12 +76,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED ?= true
 ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
-PRODUCT_PACKAGES += \
-    FaceUnlockService
-PRODUCT_SYSTEM_PROPERTIES += \
-    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+$(call inherit-product-if-exists, external/faceunlock/config.mk)
 endif
 
 # Parallel space
