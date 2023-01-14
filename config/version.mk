42d35fc2aecaa97a4fc5c1745000fd25b80e54bd
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2023-01-14 12:51:21 +0800

    config: Update versioning
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/version.mk
+++ config/version.mk
@@ -7,10 +7,19 @@ RICE_VERSION := 10.1
 RICE_CODENAME := Obbattu
 RICE_CODE := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
 
+CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
+BUILD_DATE_TIME := $(shell date -u '+%Y%m%d%H%M')
+
 ifeq ($(RICE_OFFICIAL), true)
-    RICE_BUILDTYPE := OFFICIAL
-else
-    RICE_BUILDTYPE := COMMUNITY
+   LIST = $(shell cat vendor/riceOTA/ricedroid.devices)
+    ifeq ($(filter $(CURRENT_DEVICE), $(LIST)), $(CURRENT_DEVICE))
+      IS_OFFICIAL=true
+      RICE_BUILDTYPE := OFFICIAL
+    endif
+    ifneq ($(IS_OFFICIAL), true)
+       RICE_BUILDTYPE := COMMUNITY
+       $(error Device is not official "$(CURRENT_DEVICE)")
+    endif
 endif
 
 ifeq ($(WITH_GMS), true)
@@ -26,7 +35,7 @@ else
 endif
 
 # Internal version
-LINEAGE_VERSION := riceDroid-$(RICE_CODE)-$(shell date +%Y%m%d%H)-$(LINEAGE_BUILD)-v$(RICE_VERSION)-$(RICE_PACKAGE_TYPE)-$(RICE_BUILDTYPE)
+LINEAGE_VERSION := riceDroid-$(RICE_CODE)-$(BUILD_DATE_TIME)-$(LINEAGE_BUILD)-v$(RICE_VERSION)-$(RICE_PACKAGE_TYPE)-$(RICE_BUILDTYPE)
 
 # Display version
 LINEAGE_DISPLAY_VERSION := riceDroid-$(RICE_CODE)-$(LINEAGE_BUILD)-v$(RICE_VERSION)-$(RICE_PACKAGE_TYPE)-$(RICE_BUILDTYPE)
