f973ed40d6a636f06959b67c884658120ac16b31
Pratyush <codelab@pratyush.dev>
2023-01-22 13:00:16 +0800

    overlay: add camera app QrTile activity as default
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/rice.mk
+++ config/rice.mk
@@ -87,6 +87,9 @@ endif
 ifeq ($(strip $(TARGET_BUILD_GRAPHENE_CAMERA)),true)
 PRODUCT_PACKAGES += \
     GrapheneCamera
+    
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/lineage/overlay/graphene
 endif
 
 # Bootanimation
